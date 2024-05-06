aee3c9430223464db58db50dc58c8b7599e492df
Bilikisu Wunmi Olatunji <bilqiiswunmi@gmail.com>
2024-05-06 03:31:43 +0100

    ok

---
+++ Practice/bash_scripts/bash_args_start.R
@@ -0,0 +1,15 @@
+args <- commandArgs()
+cat(args, sep = "\n")
+
+
+
+
+# 
+# args <- commandArgs(trailingOnly = TRUE)
+# cat(args, sep = "\n")
+
+# 
+# args <- commandArgs(trailingOnly = TRUE)
+# myarg1 <- args[1]
+# myargs <- args[2]
+# cat(args, sep = "\n")
